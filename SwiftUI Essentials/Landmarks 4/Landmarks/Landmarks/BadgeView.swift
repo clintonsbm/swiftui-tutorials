d8b11e901a04479fc3f6444ee085e9c0af659779
Clinton de Sá Barreto Maciel <clintonsbm@gmail.com>
2020-06-29 08:30:31 -0300

    Landmarks 3 e 4

---
+++ SwiftUI Essentials/Landmarks 4/Landmarks/Landmarks/BadgeView.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeView.swift
+//  Landmarks
+//
+//  Created by Clinton Maciel on 28/06/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeView: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct BadgeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeView()
+    }
+}
